56fabcd275e35ed6e6dd24fbda57b33c2e917448
Clarissa Reeve <clarissa.reeve@gmail.com>
2020-08-27 21:16:49 -0600

    working pexels call

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 // collapsable burger when website is mobile
-$(document).ready(function() {
+$(document).ready(function () {
 
-    $(".navbar-burger").click(function() {
+    $(".navbar-burger").click(function () {
 
         $(".navbar-burger").toggleClass("is-active");
         $(".navbar-menu").toggleClass("is-active");
@@ -10,19 +10,42 @@ $(document).ready(function() {
     let mood = 'scary';
     let apiKey = 'sLjSEJizaotgTqWKmYtg8DxMVNSsjkqS';
     let limit = 10;
-    
+
     //variable for URL to api request
     const queryURL = 'https://api.giphy.com/v1/gifs/search?q=' + mood + '&limit=' + limit + '&api_key=' + apiKey;
-    
-    //ajax call with then promise
+
+    // ajax call with then promise
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
         console.log(response);
-        for (let i = 0; i <= limit; i++) {
+        for (let i = 0; i < limit; i++) {
             console.log('Image: ' + i);
             $('#img' + i).attr('src', response.data[i].images.fixed_height_small.url);
         }
-    }); 
+    });
+
+    // pexels
+
+    const api_key = "563492ad6f9170000100000158308df764de4d088c4400082b559ea4"
+    const queryURLp = 'https://api.pexels.com/v1/search?client=' + api_key + '&query=happy&per_page=10'
+
+    function pexelSearch() {
+        $.ajax({
+            url: queryURLp,
+            method: 'GET',
+            headers: { "Authorization": api_key },
+            Default: 10,
+        }).then(function (responseP) {
+            console.log(responseP)
+            for (let i = 0; i < limit; i++) {
+                console.log('Image: ' + i);
+                $('#img' + i).attr('src', responseP.photos[i].src.small);
+            }
+        })
+    }
+
+    pexelSearch()
+
 });
